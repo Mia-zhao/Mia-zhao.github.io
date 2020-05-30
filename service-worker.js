e8e381a10ddcd47d68a616596a5fe6eb10cb78a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-30 08:08:52 +0000

    Deploy Mia-zhao/Mia-zhao.github.io to github.com/Mia-zhao/Mia-zhao.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.578811232065c017f88c4ccb74917b12.js"
+  "/precache-manifest.0db22c3d6962c3bec28192eb91aeac93.js"
 );
 
 self.addEventListener('message', (event) => {
